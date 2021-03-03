4d15ac72d61fc55fc3d2e09852e2a881b19adf68
ahui <ahui.2015@ilcoud.com>
2021-03-03 17:39:32 +0800

    添加一些文件

---
+++ mj.js
@@ -0,0 +1,17 @@
+// 以下是本框架的全部代码，使用方法请看 README.md
+
+// 函数名 m 来源于 Mithril, 也可以理解为 make 的简称，用来创建一个元素。
+function m(name) {
+  if (jQuery.type(name) == 'string') {
+    return $(document.createElement(name));
+  }
+  return name.view();
+}
+
+// 函数名 cc 意思是 create a component, 用来创建一个简单的组件。
+function cc(name, id, elements) {
+  if (!id) id = '' + Math.round(Math.random() * 100000000);
+  const vnode = m(name).attr('id', id);
+  if (elements) vnode.append(elements);
+  return {id: '#'+id, raw_id: id, view: () => vnode};
+}
